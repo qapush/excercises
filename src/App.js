1dfff09787789679817361f858e0cece3286ac00
qapush <mihail.kapush@gmail.com>
2022-01-11 20:13:09 +0100

    React useRef excercise

--- src/App.js
+++ src/App.js
@@ -1,140 +1,39 @@
-import { Component, useState, useEffect, useCallback } from "react";
-import { Container } from "react-bootstrap";
-import "./App.css";
-// class Slider extends Component {
+import {useRef, useState, useEffect} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
+const Form = () => {
 
-//     componentDidMount(){
-//         document.title = `Slide: ${this.state.slide}`
-//     }
+    const [text, setText] = useState('');
 
-//     componentDidUpdate(){
-//         document.title = `Slide: ${this.state.slide}`
-//     }
+    const myRef = useRef(1);
 
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }))
-//     }
-
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }))
-//     }
-
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
-//                 </div>
-//             </Container>
-//         )
-//     }
-// }
-
-
-
-const Slider = (props) => {
-
-  const [slide, setSlide] = useState(0);
-  const [autoplay, setAutoplay] = useState(false);
-
-  const getSomeImages = useCallback(
-      () => {
-            console.log("getting images");
-            return [
-            "https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-            "https://media.timeout.com/images/105741309/image.jpg",
-            ];
-      },[slide])
-
-  function changeSlide(i) {
-    setSlide((slide) => slide + i);
-  }
-
-  function toggleAutoplay() {
-    setAutoplay((autoplay) => !autoplay);
-  }
-
-  return (
-    <Container>
-      <div className="slider w-50 m-auto">
-        {/* {getSomeImages().map((url, i) => {
-          return (
-            <img key={i} className="d-block w-100" src={url} alt="slide" />
-          );
-        })} */}
-
-        <Slides getSomeImages={getSomeImages}/>
-
-        <div className="text-center mt-5">
-          Active slide {slide} <br />
-          {autoplay ? "auto" : null}
-        </div>
-        <div className="buttons mt-3">
-          <button
-            className="btn btn-primary me-2"
-            onClick={() => changeSlide(-1)}
-          >
-            -1
-          </button>
-          <button
-            className="btn btn-primary me-2"
-            onClick={() => changeSlide(1)}
-          >
-            +1
-          </button>
-          <button
-            className="btn btn-primary me-2"
-            onClick={toggleAutoplay}
-          >
-            Toggle autoplay
-          </button>
-        </div>
-      </div>
-    </Container>
-  );
-};
-
-const Slides = ({getSomeImages}) => {
-    const [images, setImages] = useState([]);
-    
     useEffect(()=> {
-        setImages(getSomeImages())   
-    }, [getSomeImages])
+        myRef.current++
+        console.log(myRef.current);
+    })
+
 
     return (
-        <>
-            {images.map( (url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
-        </>
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
     )
 }
 
 function App() {
-  const [slider, setSlider] = useState(true);
-  return <>{slider ? <Slider /> : null}</>;
+    return (
+        <Form/>
+    );
 }
 
 export default App;
