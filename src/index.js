314dd0b15f48761bcd695c081ac68e9f533e72cc
qapush <mihail.kapush@gmail.com>
2022-04-03 15:40:03 +0200

    Redux connect introduction

--- src/index.js
+++ src/index.js
@@ -2,47 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import * as actions from './actions';
+import App from './components/App';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-
-document.getElementById('inc').addEventListener('click', inc)
-document.getElementById('dec').addEventListener('click', dec)
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value)
-})
-
-
-
-
-
-
-
-
-// let state = reducer(undefined, { type: 'INC' });
-// console.log(state)
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
