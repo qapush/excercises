82fe587d4b60f7da54a438e51ce1c9e0443a86d3
qapush <mihail.kapush@gmail.com>
2022-04-04 20:40:07 +0200

    Redux hooks introduction

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,36 +1,30 @@
 import { Component } from "react";
 import { connect } from "react-redux";
-import * as actions from '../actions'
+import {inc, dec, rnd} from '../actions'
 import { bindActionCreators } from "redux";
+import { useSelector, useDispatch } from "react-redux";
 
-// const Counter = ({ counter, inc, dec, rnd }) => {
-//     return (
-//         <div className="jumbotron">
-//             <h1>{counter}</h1>
-//             <button onClick={inc} className="btn btn-primary">INC</button>
-//             <button onClick={dec} className="btn btn-primary">DEC</button>
-//             <button onClick={rnd} className="btn btn-primary">RND</button>
-//         </div>
-//     )
-// }
+const Counter = () => {
 
-class Counter extends Component{
-    render() {
-        const { counter, inc, dec, rnd } = this.props;
-        return (
-            <div className="jumbotron">
-                <h1>{counter}</h1>
-                <button onClick={inc} className="btn btn-primary">INC</button>
-                <button onClick={dec} className="btn btn-primary">DEC</button>
-                <button onClick={rnd} className="btn btn-primary">RND</button>
-            </div>
-        )
-    }
-} 
-const mapStateToProps = state => {
-    return {
-        counter: state.value
-    }
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="jumbotron">
+            <h1>{counter}</h1>
+            <button onClick={()=> dispatch(inc())} className="btn btn-primary">INC</button>
+            <button onClick={()=> dispatch(dec())} className="btn btn-primary">DEC</button>
+            <button onClick={()=> dispatch(rnd())} className="btn btn-primary">RND</button>
+        </div>
+    )
 }
 
-export default connect(mapStateToProps, actions)(Counter);
+
+// const mapStateToProps = state => {
+//     return {
+//         counter: state.value
+//     }
+// }
+
+// export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
