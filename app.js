0ecfb474a56790fb4045da7a7f29a141b6119f88
qapush <mihail.kapush@gmail.com>
2022-05-10 16:27:35 +0200

    Trying to find a solution still

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ function checkCashRegister(price, cash, cid) {
         "FIVE": 5,
         "TEN": 10,
         "TWENTY": 20,
-        "ONE HUNDRE": 100
+        "ONE HUNDRED": 100
     }
+
+    const parsedCid = cid.map( item => {
+        
+        const name = item[0]
+        const number = item[1]
+
+        return [name, number]
+    })
+
+
+
 
 
     let change = {
@@ -19,15 +30,15 @@ function checkCashRegister(price, cash, cid) {
     };
 
 
-    let changeDue = cash - price;
+    let changeDue = Number((cash - price).toFixed(2));
     const cashAvailable = cid.reduce((a,b)=>{
         return a + b[1]
       }, 0).toFixed(2);
 
     // RETURN 'CLOSED
     if (changeDue == cashAvailable){
-        change.status = 'CLOSED',
-        change.change = [...cid]
+        change.status = 'CLOSED';
+        change.change = [...cid];
         return change;
     }
 
@@ -39,25 +50,43 @@ function checkCashRegister(price, cash, cid) {
     // console.log('Cid available before: ', cidAvailable, '\n\n')
 
 
+    let changeArray = []
     cidAvailable.forEach( item => {
-      
-        if( item[1] <= changeDue){
-            do {
-                const amount = AMOUNTS[item[0]]
-                item[1] = parseFloat((item[1] - amount).toFixed(2));
-                changeDue = parseFloat((changeDue - amount).toFixed(2));
-                
-            } while(item[1] > 0)
-        }
         
         
 
+        if( item[1] > 0 && AMOUNTS[item[0]] <= changeDue){
+            let changeItem = [item[0], 0]
+
+            do {
+                // changeItem[1] = parseFloat(changeItem[1] + AMOUNTS[item[0]]).toFixed(2)
+                changeItem[1] += AMOUNTS[item[0]]
+                changeDue = parseFloat(changeDue - AMOUNTS[item[0]]).toFixed(2)
+                item[1] = parseFloat(item[1] - AMOUNTS[item[0]]).toFixed(2)
+            } while ( item[1] > 0 && changeDue > 0)
+
+            changeArray.push(changeItem)
+        }
+    
     })
+
+    console.log('Change array:', changeArray)
+    // console.log('Change due:', changeDue)
+
+    // RETURN 'OPEN'
+    if (changeDue == 0){
+        change.status = 'OPEN';
+        change.change = [...changeArray]
+    } else {
+        change.status = 'INSUFFICIENT_FUNDS';
+    }
 
     // console.log('Change due after: ', changeDue, '\n\n')
     // console.log('Cid available after: ',cidAvailable, '\n')
     
     
+
+    console.log(change)
     return change;
 
 
@@ -65,12 +94,19 @@ function checkCashRegister(price, cash, cid) {
   
 
 
-// OPEN
+// // OPEN
 // checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 
 // INSUFFICIENT_FUNDS
-checkCashRegister(19.5, 20, [["PENNY", 0.04], ["NICKEL", 0.00], ["DIME", 0.20], ["QUARTER", 0.25], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 
-// CLOSED
-// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 
+// INSUFFICIENT_FUNDS
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+
+
+// OPEN
+
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+
+// {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
