c9eab7649cafd9ebf9f226df31a364e5ff9be9e4
qapush <mihail.kapush@gmail.com>
2022-05-27 19:40:21 +0200

    netlify serverles simple example

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+
+function App() {
+  const [message, setMessage] = useState('text');
+  const fetchData = async () => {
+    const result = await axios.get('/.netlify/functions/helloWorld')
+    setMessage(result.data.message)
+  }
+  
+  
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {message}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
