25ae64654d788924ef59f3e59ea6ab9a601ea9b5
qapush <mihail.kapush@gmail.com>
2022-10-07 11:42:54 +0200

    Lifting state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import nextId from "react-id-generator";
+import nextId from 'react-id-generator';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -8,8 +8,8 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       data: [
         {
@@ -17,60 +17,82 @@ class App extends Component {
           salary: 800,
           increase: false,
           id: 1,
+          rise: false,
         },
         {
           name: 'Helen',
           salary: 2000,
           increase: true,
           id: 2,
+          rise: false,
         },
         {
           name: 'Eve',
           salary: 5000,
           increase: false,
           id: 3,
+          rise: false,
         },
-      ]
-    }
+      ],
+    };
     this.maxId = 4;
   }
 
   deleteItem = (id) => {
-    this.setState(({data})=>{
-    return({
-        data: data.filter(item => item.id !== id)
-      })
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
     });
-    
-  }
+  };
 
   addItem = (name, salary) => {
     const newItem = {
-        name, 
-        salary,
-        increase: false,
-        id: this.maxId++
-    }
-    this.setState(({data}) => {
-        const newArr = [...data, newItem];
-        return {
-            data: newArr
-        }
+      name,
+      salary,
+      increase: false,
+      rise: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
+      };
     });
-}
+  };
 
-  render(){
-    
+  onToggleIncrease = (id) => {
+    this.setState(({ data }) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return { ...item, increase: !item.increase };
+        }
+        return item;
+      }),
+    }));
+  };
+
+  onToggleRise = (id) => {
+    console.log('Rise this ', id);
+  };
+
+  render() {
     return (
       <div className="app">
         <AppInfo />
-  
+
         <div className="search-panel">
           <SearchPanel />
           <AppFilter />
         </div>
-        <EmployeesList data={this.state.data} onDelete={this.deleteItem}/>
-        <EmployeesAddForm onAdd={this.addItem}/>
+        <EmployeesList
+          data={this.state.data}
+          onDelete={this.deleteItem}
+          onToggleIncrease={this.onToggleIncrease}
+          onToggleRise={this.onToggleRise}
+        />
+        <EmployeesAddForm onAdd={this.addItem} />
       </div>
     );
   }
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,14 +2,18 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = ({ data, onDelete }) => {
+const EmployeesList = ({ data, onDelete, onToggleIncrease, onToggleRise }) => {
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
-    return <EmployeesListItem 
-      {...itemProps} 
-      key={ id} 
-      onDelete={() => onDelete(id)}
-      />;
+    return (
+      <EmployeesListItem
+        {...itemProps}
+        key={id}
+        onDelete={() => onDelete(id)}
+        onToggleIncrease={() => onToggleIncrease(id)}
+        onToggleRise={() => onToggleRise(id)}
+      />
+    );
   });
 
   return <ul className="app-list list-group">{elements}</ul>;
