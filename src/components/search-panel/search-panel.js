2813ccb9f5a4336f250212c81787a3a326c213fe
Michael Kapush <mihail.kapush@gmail.com>
2023-01-23 20:19:50 +0100

    ES6 module demo

--- src/components/search-panel/search-panel.js
+++
@@ -1,7 +0,0 @@
-import './search-panel.css';
-
-const SearchPanel = () => {
-  return <input type="text" className="form-control search-input" placeholder="Find employee" />;
-};
-
-export default SearchPanel;
