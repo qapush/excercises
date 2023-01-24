0359bb175b13e3550f5208b1649afe1984320c28
Michael Kapush <mihail.kapush@gmail.com>
2023-01-24 22:58:45 +0100

    Eloquent JS tasks

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
-import { hello } from "./module.js";
+// Functions Exercises
 
-hello();
+// Minimum
+
+function min(a, b){
+    return a < b ? a : b;
+}
+
+console.log(min(0, 10));
+console.log(min(0, -10));
+
+// Recursion
+
+function isEven(num){
+    switch(num){
+        case 0:
+            return true;
+            break;
+        case 1:
+            return false;
+            break;
+        default:
+            return isEven(num-2);
+            break;
+    }    
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+
+// Bean counting
+
+function countBs(str){
+    let j = '';
+    for(let i = 0; i < str.length; i++){
+        str[i] === 'B' ? j++ : null;
+    }
+    return j;
+}   
+
+console.log(countBs("BBC"));
