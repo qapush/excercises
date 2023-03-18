2bbd01a26be0d209bf6e37810d66409da5f13426
Michael Kapush <mihail.kapush@gmail.com>
2023-03-18 09:19:00 +0100

    Redux tutorial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+import './api/server'
+
+import store from './store'
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+// log: 'Hi!'
+
+console.log('State after dispatch: ', store.getState())
+// log: {todos: [...], filters: {status, colors}, meaningOfLife: 42}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
