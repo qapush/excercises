49590531ac26837eb38a912ee08f3d7bcda296ac
qapush <mihail.kapush@gmail.com>
2023-05-02 12:11:19 +0200

    Browser router6 intro

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is test project with hero admin panel!
+Repository created to do and commit different exercises
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
-import App from './components/app/App';
-import store from './store';
-
-import './styles/index.scss';
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
