6ed6eb7bc7850b8f1e56220d6d68b765d8976843
qapush <mihail.kapush@gmail.com>
2024-08-01 18:46:58 +0200

    repeat router cont

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,34 +1,33 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
-import Search from "./Search";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectArticles, filterArticles } from '../features/articles/articlesSlice';
+import Search from './Search';
+import { Link } from 'react-router-dom';
 
 // Import Link and useSearchParams from React Router
 
-export default function Articles () {
+export default function Articles() {
   const articles = useSelector(selectArticles);
 
   // Grab URLSearchParams object from useSearchParams hook
-  
+
   // Get the queryParams from object returned from useSearchParams and set to `title`
   const title = '';
 
-  const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
+  const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles);
 
   return (
     <main>
       <h1>Articles</h1>
       <ul>
-        { filteredArticles.map(article => (
+        {filteredArticles.map((article) => (
           <li key={article.slug}>
             {/* Replace these a tags! */}
-            <a href={`${article.slug}`}>
-              {article.title}
-            </a>
+            <Link to={`${article.slug}`}>{article.title}</Link>
           </li>
         ))}
       </ul>
       <Search />
     </main>
-  )
+  );
 }
